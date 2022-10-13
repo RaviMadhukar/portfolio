dc555bfa36b4bb6c2f8e08b19dd41dbc91b2ca03
RAvi <7ravi6kumar@gmail.com>
2022-10-13 14:06:33 +0530

    project completed

--- script.js
+++ script.js
@@ -9,17 +9,23 @@ function opentab(tabname) {
         tabcontent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab")
+    document.getElementById(tabname).classList.add("active-tab");
 }
 
 var sidemuneu = document.getElementById("sidemenu");
 
 function openmenu() {
     sidemuneu.style.right = "0";
+    document.getElementById("openn").classList.remove("active-bar");
+    document.getElementById("closee").classList.add("active-bar");
+
 }
 
 function closemenu() {
-    sidemuneu.style.right = "-250px";
+    sidemuneu.style.right = "-253px";
+        document.getElementById("closee").classList.remove("active-bar");
+        document.getElementById("openn").classList.add("active-bar");
+
 }
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxBWa0MqHcCUtwr5nuNT6-ZgiYXGwSb4I6IB734rZOTtUATdvO0kw2DWfoNJ62PK8CE4A/exec'
